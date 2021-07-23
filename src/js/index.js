9d58c0cea7d9577768dba1b90acf887be589e853
toufik1998 <toufikshima98@gmail.com>
2021-07-23 06:20:22 +0100

    add two section

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,30 @@ import '../sass/style.scss';
 import '@fortawesome/fontawesome-free/js/all.min';
 
 
+$(document).ready(function(){
+    $(".nav-item").click(function(){
+        // احدف الصنف active من الزر الدي لديه الصنف nav-item 
+        $(".nav-item").removeClass("active");
+        // اضف الصنف active الى الزر الدي ضغط عليه 
+        $(this).addClass("active");
+      });
+
+    $("a.scroll").on("click", function(event){
+        var hash = this.hash;
+        $("html, body").animate({scrollTop: $(hash) .offset() .top - 100}, 800, function(){});
+    });
+
+
+    $(function(){
+        $("#commentform").validate();
+    });
+
+    var date = new Date();
+    var year = date.getFullYear();
+    document.getElementById('date').innerHTML = year;
+
+});
+
+
 
 
